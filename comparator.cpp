5fe21a86178ccad8b2f3e208287564a07596b8a5
bayazit578 <rakh.mat@inbox.ru>
2025-09-28 20:55:46 +0300

    использует длину строки

--- comparator.cpp
+++ comparator.cpp
@@ -7,13 +7,13 @@
 #include "struct.h"
 
 int CompareStr(const void* par1, const void* par2) {
-    string* str1 = (string*)par1;
-    string* str2 = (string*)par2;
-    return My_Strcmp(*str1, *str2);
+    string str1 = *(string*)par1;
+    string str2 = *(string*)par2;
+    return My_Strcmp(str1, str2);
 }
 
 int CompareStr_back(const void* par1, const void* par2) {
-    string* str1 = (string*)par1;
-    string* str2 = (string*)par2;
-    return My_Strcmp_back(*str1, *str2);
+    string str1 = *(string*)par1;
+    string str2 = *(string*)par2;
+    return My_Strcmp_back(str1, str2);
 }
